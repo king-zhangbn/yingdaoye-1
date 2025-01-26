b8d62365b075c7a2277ab0bb9d20d13e7c273e8c
king-zhangbn <zhangbingnan01@gmail.com>
2025-01-26 22:10:15 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,21 +11,23 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
     site: "https://en.icu",
-    adapter: vercel(),
-    redirects: {
-        "/admin": "/keystatic",
-    },
-    integrations: [
-        react(),
-        keystatic(),
-        tailwind(),
-        sitemap(),
-        compress({
-            HTML: true,
-            JavaScript: true,
-            CSS: true,
-            Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-            SVG: false, // astro-icon handles this
-        }),
-    ],
+	adapter: vercel({	
+		imageCDN: false,
+	}),
+	redirects: {
+		"/admin": "/keystatic",
+	},
+	integrations: [
+		react(),
+		keystatic(),
+		tailwind(),	
+		sitemap(),
+		compress({
+			HTML: true,
+			JavaScript: true,
+			CSS: true,
+			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+			SVG: false, // astro-icon handles this
+		}),
+	],
 });
