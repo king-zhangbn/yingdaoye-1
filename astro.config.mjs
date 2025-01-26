e860dca847d89f353339f47a12b04e157fac8229
king-zhangbn <zhangbingnan01@gmail.com>
2025-01-26 22:08:06 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,31 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "@playform/compress";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://landingpad.cosmicthemes.com/",
-	adapter: netlify({
-		imageCDN: false,
-	}),
-	redirects: {
-		"/admin": "/keystatic",
-	},
-	integrations: [
-		react(),
-		keystatic(),
-		tailwind(),
-		sitemap(),
-		compress({
-			HTML: true,
-			JavaScript: true,
-			CSS: true,
-			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
-			SVG: false, // astro-icon handles this
-		}),
-	],
+    site: "https://en.icu",
+    adapter: vercel(),
+    redirects: {
+        "/admin": "/keystatic",
+    },
+    integrations: [
+        react(),
+        keystatic(),
+        tailwind(),
+        sitemap(),
+        compress({
+            HTML: true,
+            JavaScript: true,
+            CSS: true,
+            Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+            SVG: false, // astro-icon handles this
+        }),
+    ],
 });
